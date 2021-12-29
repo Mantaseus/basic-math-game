3121f79468f8f00e751b39e10edf3fed0fc93690
Satnam Singh Brar <brar.satnam1995@gmail.com>
2021-12-29 14:54:20 -0800

    feat: randomly generate questions
    - They are not displayed proerly to the user yet though

--- src/number_game.ts
+++ src/number_game.ts
@@ -5,7 +5,54 @@ async function askQuestion(question: string) {
   return await new Promise<string>(res => rl.question(question, res));
 }
 
+const COUNTDOWN_START = 3;
+const OPERATIONS = (['+', '-', 'x', '/'] as const).slice();
+
+interface Question {
+  operand1: number;
+  operand2: number;
+  operation: typeof OPERATIONS[number];
+  result: number;
+}
+
 (async () => {
-  console.log(await askQuestion('Say something'));
+  const questions = Array(5).fill(0).map<Question>(() => {
+    const operation = OPERATIONS[Math.floor(Math.random() * OPERATIONS.length)];
+    switch(operation) {
+      case '+': {
+        const operand1 = Math.floor(Math.random() * 10);
+        const operand2 = Math.floor(Math.random() * 10);
+        const result = operand1 + operand2;
+        return { operand1, operand2, operation, result };
+      }
+      case '-': {
+        const result = Math.floor(Math.random() * 10);
+        const operand2 = Math.floor(Math.random() * 10);
+        const operand1 = result + operand2;
+        return { operand1, operand2, operation, result };
+      }
+      case 'x': {
+        const operand1 = Math.floor(Math.random() * 10);
+        const operand2 = Math.floor(Math.random() * 10);
+        const result = operand1 * operand2;
+        return { operand1, operand2, operation, result };
+      }
+      case '/': {
+        const result = Math.floor(Math.random() * 10);
+        const operand2 = Math.floor(Math.random() * 10);
+        const operand1 = result * operand2;
+        return { operand1, operand2, operation, result };
+      }
+    }
+  });
+  console.log(questions)
+  return;
+
+  for (let i = COUNTDOWN_START; i > 0; i--) {
+    console.log(`Get Ready... ${i}`);
+    await new Promise(res => setTimeout(res, 1000));
+  }
+
+  console.log(await askQuestion('Say something: '));
   process.exit();
 })();
